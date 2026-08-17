486230a1fbef1cb002a253d79800436b654670a7
Shivabasav Murgod <murgodshivabasav@gmail.com>
2026-08-17 12:21:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+let cartCount = 0;
 
+const cartCountElement = document.getElementById("cart-count");
+const addCartButtons = document.querySelectorAll(".add-cart");
+
+addCartButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        cartCount++;
+        cartCountElement.textContent = cartCount;
+
+        const productName = button.dataset.name;
+
+        alert(productName + " added to cart!");
+    });
+});
